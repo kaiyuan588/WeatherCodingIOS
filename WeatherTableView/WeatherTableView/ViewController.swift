4e2e7e5ebe179fb15ab33832a4531bd107810cce
kaiyuan zhao <zhao.kaiy@husky.neu.edu>
2019-12-09 23:15:04 -0500

    install pod file

--- WeatherTableView/WeatherTableView/ViewController.swift
+++ WeatherTableView/WeatherTableView/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        return cell
+    }
 }
 
